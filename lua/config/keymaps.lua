7a73ec9fa653b5dbb5d7d476faacce60ae9d30a6
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-07-02 16:47:01 -0600

    modified:   LICENSE 	modified:   README.md 	new file:   init.lua 	new file:   lazy-lock.json 	new file:   lazyvim.json 	new file:   lua/config/autocmds.lua 	new file:   lua/config/keymaps.lua 	new file:   lua/config/lazy.lua 	new file:   lua/config/options.lua 	new file:   lua/plugins/alpha.lua 	new file:   lua/plugins/example.lua 	new file:   lua/plugins/neocord.lua 	new file:   lua/plugins/oil.lua 	new file:   static/gigachad.gif 	new file:   stylua.toml

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- vim.keymap.set("n", "<leader>cx", "<cmd>terminal<CR>")
+vim.keymap.set("n", "<leader>cx", "<cmd>close<CR>", { desc = "Close tab" })
+
+-- oil.nvim
+vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory on floating window" })
